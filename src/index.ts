31393a06f73e25b7f6dffbbdea7be0586041ca16
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2024-03-28 15:23:22 +0300

    remove startapp export

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import app from "./setting";
 import {runDb} from "./repositories/dbConfig";
 import 'dotenv/config';
 
-const port: string | undefined | number = process.env.PORT || 3000;
+const port: string = process.env.PORT!;
 
-runDb().then(() => {
+const startApp = async () => {
+    await runDb()
     app.listen(port, () => {
         console.log(`app listening on port ${port}`)
     })
-})
+}
+startApp()
