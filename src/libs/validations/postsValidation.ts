aa8a400d2dab2014b843f206c21f9d2c7fc60bf4
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2024-05-08 08:00:09 +0300

    fix todo

--- src/libs/validations/postsValidation.ts
+++ src/libs/validations/postsValidation.ts
@@ -1,16 +1,16 @@
 import {body, ValidationChain} from "express-validator";
 import {Blog} from "../types/blogsTypes";
-import {blogsCollection} from "../../repositories/dbConfig";
+import blogsRepository from "../../repositories/blogsRepository";
+import {REPOSITORY_RESPONSES} from "../common/constants/repositoryResponse";
 
 const postsValidationChain: ValidationChain[] = [
     body('title').isString().bail().trim().notEmpty().bail().isLength({min: 1, max: 30}),
     body('shortDescription').isString().bail().trim().notEmpty().bail().isLength({min: 1, max: 100}),
     body('content').isString().bail().trim().notEmpty().bail().isLength({min: 1, max: 1000}),
     body('blogId').isString().bail().trim().notEmpty().bail().custom(async (blogId, {req}) => {
-        //TODO: не надо к коллекции обращаться, нужно к репозиторию
-        const foundBlog: Blog | null = await blogsCollection.findOne({id: blogId})
-        // Если блога с таким blogId не существует, то вернем ошибку
-        if (!foundBlog) {
+            const foundBlog: Blog | REPOSITORY_RESPONSES.NOT_FOUND | REPOSITORY_RESPONSES.UNSUCCESSFULLY = await blogsRepository.getBlogById(blogId)
+        // Если блога с таким blogId не существует или возникла серверная ошибка при запросе в БД, то вернем ошибку
+        if (foundBlog === REPOSITORY_RESPONSES.NOT_FOUND || foundBlog === REPOSITORY_RESPONSES.UNSUCCESSFULLY) {
             throw new Error();
         }
         // в postsService нужен будет blogName. Чтобы не искать блог второй раз дополняем req.body здесь
