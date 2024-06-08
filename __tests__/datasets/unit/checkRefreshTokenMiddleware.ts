4cd93c870b68591dbbea074f49fdffb621e0b3f6
Melnykov Dmitry Hobby <melnykov.ds99@gmail.com>
2024-06-08 10:47:46 +0300

    add tests for checkRefreshTokenMiddleware

---
+++ __tests__/datasets/unit/checkRefreshTokenMiddleware.ts
@@ -0,0 +1,37 @@
+const validRefreshToken: string = "Bearer valid_jwt_token";
+const invalidRefreshToken: string = "Bearer invalidToken";
+const validUserId: string = "12345678-aaaa-bbbb-cccc-12345678acbd";
+const validDeviceId: string = "87654321-aaaa-bbbb-cccc-87654321acbd";
+const expiredRefreshToken: string = "Bearer expired_refresh_token";
+const refreshTokenInBlacklist: string = "87654321-aaaa-aaaa-aaaa-87654321acbd";
+const decodedExpiredRefreshToken = {
+    exp: (Date.now() / 1000) - 3600, // Токен просрочен на час
+    iat: (Date.now() / 1000) - 7200, // Токен создан 2 часа назад
+    userId: validUserId,
+    deviceId: validDeviceId,
+};
+const decodedRefreshTokenWithoutUserId = {
+    exp: Math.floor(Date.now() / 1000) + 3600, // Токен действует еще час
+    deviceId: validDeviceId,
+};
+const decodedRefreshTokenWithoutDeviceId = {
+    exp: Math.floor(Date.now() / 1000) + 3600,
+    userId: validUserId,
+};
+const decodedValidToken = {
+    exp: (Date.now() / 1000) + 3600,
+    iat: (Date.now() / 1000) - 3600,
+    userId: validUserId,
+    deviceId: validDeviceId,
+};
+const validDeviceData = {
+    ip: "1.1.1.1",
+    title: "device 1",
+    lastActiveDate: "2024-01-01T05:00:00.000Z",
+    iatRefreshToken: (Date.now() / 1000) - 3600,
+    expRefreshToken: (Date.now() / 1000) + 3600,
+    userId: validUserId,
+    deviceId: validDeviceId,
+};
+
+export {validRefreshToken, invalidRefreshToken, expiredRefreshToken, validUserId, validDeviceId, decodedValidToken, refreshTokenInBlacklist, validDeviceData, decodedExpiredRefreshToken, decodedRefreshTokenWithoutUserId, decodedRefreshTokenWithoutDeviceId}
